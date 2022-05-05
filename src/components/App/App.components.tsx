2512dfcb4022a1b3e247ab7ef026d5b23feb065e
Arkadiusz Milewski <asmilewski@gmail.com>
2022-05-05 12:54:15 +0200

    styling components added, first form scaffolding done

--- src/components/App/App.components.tsx
+++ src/components/App/App.components.tsx
@@ -1,8 +1,13 @@
+import React from 'react'
+import ColorForm from '../ColorForm/ColorForm.components'
+import './App.style.scss'
+
 
 const App = () => {
   return (
     <div className="App">
       This is a new component
+      <ColorForm />
     </div>
   );
 }
--- src/components/ColorForm/ColorForm.components.tsx
+++ src/components/ColorForm/ColorForm.components.tsx
@@ -1,1 +1,15 @@
-export { };
+const ColorForm = () => {
+  return (
+    <div>
+      <form>
+        <label>
+          <h1>Choose color</h1>
+          <input></input>
+          <button>Submit</button>
+        </label>
+      </form>
+    </div>
+  )
+}
+
+export default ColorForm;
